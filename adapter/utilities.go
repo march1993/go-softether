0f3b4d984db8cfdb1f2c72f3d40efef91aa3170e
march1993 <125931004@qq.com>
2021-02-15 22:52:04 +0800

    remove debug output

--- adapter/utilities.go
+++ adapter/utilities.go
@@ -5,13 +5,14 @@ import (
 	"net"
 )
 
+const debug = false
+
 func printPackets(name string, p []Packet) {
-	fmt.Println(name, len(p), "packet(s)")
-	for idx, p := range p {
-		dst, src := net.HardwareAddr(p[:6]).String(), net.HardwareAddr(p[6:12]).String()
-		if dst == "5e:d4:ed:6f:fb:0c" || src == "5e:d4:ed:6f:fb:0c" {
+	if debug {
+		fmt.Println(name, len(p), "packet(s)")
+		for idx, p := range p {
+			dst, src := net.HardwareAddr(p[:6]).String(), net.HardwareAddr(p[6:12]).String()
 			fmt.Println("#", idx, "len:", len(p), src, "=>", dst)
 		}
-
 	}
 }
